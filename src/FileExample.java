867d6caf4e4e4374be836c65c086da68e3a495f3
Дмитрий <trololodim69@gmail.com>
2022-07-05 00:51:25 +0300

    test

--- src/FileExample.java
+++ src/FileExample.java
@@ -7,20 +7,20 @@ import java.util.function.Supplier;
 
 public class FileExample {
     public static void main(String[] args) {
-        Supplier<Add> supplier = Add::new;
-        Add add = supplier.get();
+        Supplier<User> supplier = User::new;
+        User user = supplier.get();
 
         Supplier<Thread> threadSupplier = () -> {
-            Thread thread = new Thread(add);
-            thread.setName("test"+ new Random().nextInt(100));
+            Thread thread = new Thread(user);
+            thread.setName("user_nickname"+ new Random().nextInt(100));
             return thread;
         };
-        Thread thread1 = threadSupplier.get();
-        Thread thread2 = threadSupplier.get();
-        Thread thread3 = threadSupplier.get();
-        thread1.start();
-        thread2.start();
-        thread3.start();
+        Thread user1 = threadSupplier.get();
+        Thread user2 = threadSupplier.get();
+        Thread user3 = threadSupplier.get();
+        user1.start();
+        user2.start();
+        user3.start();
 
     }
 
@@ -30,7 +30,7 @@ public class FileExample {
         String string;
         try (BufferedReader reader = Files.newBufferedReader(path)){
             String line = reader.readLine();
-            System.out.println(line + "   " +  Thread.currentThread().getName() + "read this file");
+            System.out.println(line + "   " +  Thread.currentThread().getName() + " read this file");
             string = line;
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -48,10 +48,13 @@ public class FileExample {
     }
 }
 
-class Add implements Runnable{
+class User implements Runnable{
+
+    private int value = 0;
 
     @Override
     public void run() {
+        doSome();
         doSome();
     }
 
@@ -60,6 +63,9 @@ class Add implements Runnable{
         int i = Integer.parseInt(read);
         String line = i + 10 + "";
         FileExample.write(line);
+        this.value = i;
+        System.out.println(Thread.currentThread().getName() + "change value from "+ this.value + " to " + (i+10));
+
     }
 
 
